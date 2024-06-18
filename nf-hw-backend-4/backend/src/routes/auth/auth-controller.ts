6ec237f098ab9c549305c1e13f5c5e180de8c817
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-06-18 08:29:55 +0600

    addSettingsMoreMore3

--- nf-hw-backend-4/backend/src/routes/auth/auth-controller.ts
+++ nf-hw-backend-4/backend/src/routes/auth/auth-controller.ts
@@ -29,7 +29,7 @@ class AuthController {
       }
       res.status(200).json(result)
     } catch (err) {
-      res.status(500).json({ message: 'Error logging in' })
+      res.status(500).json({ message: 'Error logging in' + err })
     }
   }
 
