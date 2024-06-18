c115186df613f1cc0e2a98caadd333fd4bba5784
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-06-18 11:24:47 +0600

    fixBugs

--- nf-hw-backend-4/frontend/src/components/Artistsection/index.jsx
+++ nf-hw-backend-4/frontend/src/components/Artistsection/index.jsx
@@ -37,7 +37,7 @@ export const Artistsection = ({ title }) => {
         </Link>
       </div>
       <div className="horizontal-scroll grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
-        {artists.map((artist, index) => (
+        {artists && artists.map((artist, index) => (
           artist.popular && index < 6 ? <ArtistsCard
           key={index}
           id={artist._id}
